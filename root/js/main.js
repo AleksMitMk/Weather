0bf014f15103165ce9f520498c32524ba1bb612e
Aleksandar Mitev <57228808+AleksMitMk@users.noreply.github.com>
2021-03-05 16:41:24 +0100

    Added images and some css styles

--- root/js/main.js
+++ root/js/main.js
@@ -9,9 +9,9 @@ const weatherData = {
         { "day": "Tue", "temp": "14", "windDirection": "north-west", "windSpeed": "14", "type": "rainy" },
         { "day": "Wed", "temp": "17", "windDirection": "south-east", "windSpeed": "20", "type": "cloudy" },
         { "day": "Thu", "temp": "32", "windDirection": "south-west", "windSpeed": "20", "type": "windy" },
-        { "day": "Fri", "temp": "23", "windDirection": "south", "windSpeed": "20", "type": "stormy" },
-        { "day": "Sat", "temp": "11", "windDirection": "east", "windSpeed": "20", "type": "snowy" },
-        { "day": "Sun", "temp": "5", "windDirection": "west", "windSpeed": "20", "type": "sunny" },
+        { "day": "Fri", "temp": "23", "windDirection": "south-east", "windSpeed": "20", "type": "stormy" },
+        { "day": "Sat", "temp": "11", "windDirection": "south-west", "windSpeed": "20", "type": "snowy" },
+        { "day": "Sun", "temp": "5", "windDirection": "north-west", "windSpeed": "20", "type": "sunny" },
     ]
 };
 
@@ -19,20 +19,57 @@ const weatherData = {
 // Function to fill the HTML with cards
 
 let container = document.querySelector('.weather-container');
+let card = document.querySelector('.weather-card');
+let showInfo = false;
 
 const allCards = () =>{
     
     weatherData.days.map(el => {
-            console.log(el.type)
-        container.innerHTML += `<div class="weather-card london">
+            console.log(el.windDirection)
+
+        container.innerHTML += `<div class="weather-card"> 
+                                    <div class="${el.type}">
+                                        <div class="overlay"></div>
+                                    </div>
                                     <div class="weather-icon ">
                                         <img src='./root/img/${el.type}.png' />
                                     </div>
-                                    <h1>${el.temp}º</h1>
-                                    <p>${el.day}</p>
+                                    <div class="tempAndDayInfo">
+                                        <h1>${el.temp}º</h1>
+                                        <p>${el.day}</p>
+                                    </div>
+                                    <div class="${showInfo ? 'hiddenInfo  show': 'hiddenInfo'}">
+                                        <div class="direction">
+                                            <p>Wind Direction</p>
+                                            <h2>${el.windDirection}</h2>
+                                            <i class="fas fa-location-arrow fa-2x ${el.windDirection}"></i>
+                                        </div>
+                                        <div>
+                                            <span>Wind Speed:</span>
+                                            <h2>${el.windSpeed} ${weatherData.windSpeedUnit}</h2>
+                                        </div>
+                                    </div>
                                 </div>`
     })
 
 
 }
+
+// On click Toggle data on the card
+
+// console.log(card)
+const infoToggle = (e) => {
+   let card = e.target.parentNode.parentNode;
+   console.log(this)
+       if(card.classList.value == "weather-card"){
+           if(showInfo){
+               showInfo= false
+           }else{
+               showInfo = true;
+           }
+        }
+    
+    console.log(showInfo)
+}
+document.addEventListener('click', infoToggle, false);
 allCards();
